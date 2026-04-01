b02264c947f5990220493afc7cfb91ad062bdc4e
Shrikanta Dutta <120603877+Shrikanta-26@users.noreply.github.com>
2026-04-01 23:10:53 +0600

    Time: 81 ms (22.75%) | Memory: 214.8 MB (27.35%) - LeetSync

---
+++ 2846-robot-collisions/robot-collisions.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    struct Robot {
+        int pos, hp, id;
+        char dir;
+    };
+
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        int n = positions.size();
+        vector<Robot> robots;
+        
+        for (int i = 0; i < n; i++) {
+            robots.push_back({positions[i], healths[i], i, directions[i]});
+        }
+
+        // Sort by actual position on the line
+        sort(robots.begin(), robots.end(), [](const Robot& a, const Robot& b) {
+            return a.pos < b.pos;
+        });
+
+        stack<int> st; // stores index in the 'robots' vector
+        for (int i = 0; i < n; i++) {
+            if (robots[i].dir == 'R') {
+                st.push(i);
+                continue;
+            }
+
+            // Robot is moving Left, check for collisions with Right-movers
+            while (!st.empty() && robots[i].hp > 0) {
+                int j = st.top();
+                
+                if (robots[j].hp == robots[i].hp) {
+                    robots[i].hp = 0;
+                    robots[j].hp = 0;
+                    st.pop();
+                } else if (robots[j].hp > robots[i].hp) {
+                    robots[j].hp -= 1;
+                    robots[i].hp = 0;
+                } else {
+                    robots[i].hp -= 1;
+                    robots[j].hp = 0;
+                    st.pop();
+                }
+            }
+        }
+
+        // Sort back to original input order to extract healths
+        sort(robots.begin(), robots.end(), [](const Robot& a, const Robot& b) {
+            return a.id < b.id;
+        });
+
+        vector<int> res;
+        for (auto& r : robots) {
+            if (r.hp > 0) res.push_back(r.hp);
+        }
+        return res;
+    }
+};
